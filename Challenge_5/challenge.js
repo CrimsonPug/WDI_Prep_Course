e9f197d77fb4415ab7b0f78ff2693ca18c849924
Ikram Mustapha <ikrammustapha@Ikrams-MacBook-Pro.local>
2016-09-25 21:05:18 -0400

    5th

---
+++ Challenge_5/challenge.js
@@ -0,0 +1,35 @@
+// Exercise 1
+
+var name ="Ikram"
+var greeting ="Hello " + name;
+console.log(greeting);
+
+//Exercise 2
+
+var x = 1125
+var y = x%2;
+console.log(y);
+
+//Exercise 3
+var a =true;
+var b =false;
+var c =true;
+var result =a||b||c;
+console.log(result);
+
+//Exercise 4
+var a=true;
+var b=false;
+var c=true;
+var d=a&&b&&c;
+console.log(d);
+
+//Exercise 5
+var answer1 = 10 - 3 * 5/4 + 202 % 3;
+var answer2 = 10 - (3 * (5/4) + (202 % 3));
+console.log(answer1);
+console.log(answer2);
+
+//Exercise 6
+var x ='Hello World'.length;
+console.log(x);
